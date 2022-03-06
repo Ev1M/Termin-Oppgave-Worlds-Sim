c0f136ba626b136f7b4fd9b62576a25da9c6e9e8
Ev1M <90681485+Ev1M@users.noreply.github.com>
2022-03-06 20:13:08 +0100

    problems!

--- PHP/LoginScript/SignUp.php
+++ PHP/LoginScript/SignUp.php
@@ -2,23 +2,36 @@
 <html>
 <head>
     <title> Sing Up Page </title>
-    <link rel="stylesheet" href=".../css/stylesSignUpLogIn">
+    <link rel="stylesheet" href="../../css/stylesSignUpLogIn.css">
 </head>
-<div class="signup-form">
+<body>
+
+<div class="container">
+    <div class="Meny">
+      <a href="../../index.php" >Home</a>
+      <a href="login.php">Login</a>
+      <a href="../../HTML/Contact.html">Contact</a>
+      <a href="../../HTML/createForum.html">Post Forum! </a>
+        </div>
+
+
+
+<div class="Login-Form">
 
 <form action="Includes/SignUp.inc.php" method="post">
 
-<input type="text" name="name" placeholder="Full Name...">
-<input type="text" name="E-mail" placeholder="E-mail adress...">
-<input type="text" name="Uid" placeholder="Username...">
-<input type="password" name="Pwd" placeholder="Password...">
-<input type="password" name="PwdRepeat" placeholder="Repeat Password...">
+<input type="text" name="name" placeholder="Full Name..."><br>
+<input type="text" name="E-mail" placeholder="E-mail adress..."><br>
+<input type="text" name="Uid" placeholder="Username..."><br>
+<input type="password" name="Pwd" placeholder="Password..."><br>
+<input type="password" name="PwdRepeat" placeholder="Repeat Password..."><br>
 <button type="submit" value="Submit" name="submit" > Sign up </button>
 
 </form>
 
 </div>
-
+</div>
+</body>
 </html>
 <?php
 
--- PHP/LoginScript/login.php
+++ PHP/LoginScript/login.php
@@ -1,8 +1,29 @@
+<head>
+<link rel="stylesheet" href="../../css/stylesSignUpLogIn.css">
+</head>
+
+<body>
+
+<div class="container">
+<div class="Meny">
+      <a href="../../index.php" >Home</a>
+      <a href="login.php">Login</a>
+      <a href="../../HTML/Contact.html">Contact</a>
+      <a href="../../HTML/createForum.html">Post Forum! </a>
+        </div>
+
+
+
+<div class="Login-Form">
+
 <form action="Includes/Login.inc.php" method="post">
 
-<input type="text" name="Uid" placeholder="Username/E-mail...">
-<input type="password" name="Pwd" placeholder="Password...">
+<input type="text" name="Uid" placeholder="Username/E-mail..."><br>
+<input type="password" name="Pwd" placeholder="Password..."><br>
 <button type="submit" name="button"> Login </button>
-
+</div>
+</div>
 
 </form>
+
+</body>
