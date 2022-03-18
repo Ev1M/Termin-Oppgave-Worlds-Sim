92ea2bf288e68f295e8514d2c1157ac36d519f83
Ev1M <90681485+Ev1M@users.noreply.github.com>
2022-03-18 10:45:34 +0100

    Update functions.inc.php

--- PHP/LoginScript/Includes/functions.inc.php
+++ PHP/LoginScript/Includes/functions.inc.php
@@ -49,6 +49,8 @@ function PwdMatch($Pwd, $PwdRepeat){
 function uidExsist($conn, $username, $email){
  $sql = "SELECT * FROM users WHERE userUid = ? OR usersEmail = ?;";
  $stmt = mysqli_stmt_init($conn);
+ var_dump($stmt);
+ die();
  if (!mysqli_stmt_prepare($stmt, $sql)) {
     header("location: ../SignUp.php?error=stmtFailed");
     exit();
