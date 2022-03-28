1e601eeddf169487b160ad09ec638dec6c2a342f
Ev1M <90681485+Ev1M@users.noreply.github.com>
2022-03-28 14:31:19 +0200

    Update profile.php

--- PHP/LoginScript/profile.php
+++ PHP/LoginScript/profile.php
@@ -26,6 +26,9 @@
 $SQL = "SELECT * FROM forum";
 $result = mysqli_query($conn, $SQL);
 $queryResult = mysqli_num_rows($result);
+echo $result;
+var_dump($result);
+die();
 //Checks if there is any information to be written out
 if ($queryResult > 0) {
   while ($row = mysqli_fetch_assoc($result)) {
