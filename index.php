c74466714a7cd79fbaebaee9f76c0b1a34de1331
Ev1M <90681485+Ev1M@users.noreply.github.com>
2022-03-28 15:36:00 +0200

    Update index.php

--- index.php
+++ index.php
@@ -50,7 +50,6 @@
           $SQL = "SELECT * FROM forum";
           $result = mysqli_query($conn, $SQL);
           $queryResult = mysqli_num_rows($result);
-          echo "BALLS AND FUNNY AMOG";
           var_dump($result);
           die();
           //Checks if there is any information to be written out
