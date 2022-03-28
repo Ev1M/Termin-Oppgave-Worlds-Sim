372114c6d7915bd505e2f079401b7629c757c19b
Ev1M <90681485+Ev1M@users.noreply.github.com>
2022-03-28 14:35:00 +0200

    dfgh

--- index.php
+++ index.php
@@ -50,6 +50,9 @@
           $SQL = "SELECT * FROM forum";
           $result = mysqli_query($conn, $SQL);
           $queryResult = mysqli_num_rows($result);
+          echo "BALLS AND FUNNY AMOG";
+          var_dump($result);
+          die();
           //Checks if there is any information to be written out
           if ($queryResult > 0) {
             while ($row = mysqli_fetch_assoc($result)) {
